6981f484e00877afa444ff5d940db44e9ce86193
mak4515 <49048165+mak4515@users.noreply.github.com>
2020-08-11 08:33:12 -0400

    remove portables

--- runShinyApp.R
+++
@@ -1,5 +0,0 @@
-.libPaths("./R-Portable/App/R-Portable/library")
-# the path to portable chrome
-browser.path = file.path(getwd(),"GoogleChromePortable/GoogleChromePortable.exe")
-options(browser = browser.path)
-shiny::runApp("./Shiny/",port=8888,launch.browser=TRUE)

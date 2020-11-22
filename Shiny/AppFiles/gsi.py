9d3718bf39ed67cb72f7c54edaddfd3b2169c6f8
mak4515 <49048165+mak4515@users.noreply.github.com>
2020-11-22 12:09:46 -0500

    Update gsi.py

--- Shiny/AppFiles/gsi.py
+++ Shiny/AppFiles/gsi.py
@@ -6,8 +6,8 @@ def run_functions(proteins, species):
 	IDs = IDs_df.values.flatten()
 	known_interactions = interactions.get_interactions(IDs, species)
 
-	known_interactions['Original geneID_A'] = IDs_df.loc[known_interactions['ncbiTaxonId'].astype(str).str.cat(known_interactions['stringId_A'], sep='.').values, 'queryItem'].values
-	known_interactions['Original geneID_B'] = IDs_df.loc[known_interactions['ncbiTaxonId'].astype(str).str.cat(known_interactions['stringId_B'], sep='.').values, 'queryItem'].values
+	known_interactions['Original geneID_A'] = IDs_df.loc[known_interactions['stringId_A'].values, 'queryItem'].values
+	known_interactions['Original geneID_B'] = IDs_df.loc[known_interactions['stringId_B'].values, 'queryItem'].values
 
 	results = known_interactions.set_index(['Original geneID_A', 'Original geneID_B'])
 	results = results.reset_index(level=['Original geneID_A', 'Original geneID_B'])
